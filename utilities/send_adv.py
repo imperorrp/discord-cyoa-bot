53898a3fa3b5c5b18beba956edda81bd1af91d4a
Ratish Panda <ratishp2001@gmail.com>
2022-12-17 18:15:54 +0530

    Added image support for adventure embeds

--- utilities/send_adv.py
+++ utilities/send_adv.py
@@ -5,22 +5,23 @@ from discord import Button, ButtonStyle
 
 async def button_sender(ctx, advname, page, msgid = 0):
     #if msgid = 0, send new message (for 'start'ing an adv). Else edit existing embed.
-    content = page[1]
+    content = page[1].split("@img")[0]
+    image = page[1].split("@img")[1]
     options = page[2:]
     print(options)
     choice_num = len(options)
+    embed=discord.Embed(title=advname, description=content)
+    embed.set_image(url=image)
     if choice_num==1:
         label1 = options[0].split("@")[0]
         if msgid==0:
-            msg = await ctx.send(embed=discord.Embed(title=advname,
-                                     description=content), components=[[
+            msg = await ctx.send(embed, components=[[
                             Button(label=label1,
                                     custom_id="option_1",
                                     style=ButtonStyle.grey)]])
         else:
             #msg = await ctx.fetch_message(msgid)
-            await ctx.edit(embed=discord.Embed(title=advname,
-                                     description=content), components=[[
+            await ctx.edit(embed, components=[[
                             Button(label=label1,
                                     custom_id="option_1",
                                     style=ButtonStyle.grey)]])
@@ -28,8 +29,7 @@ async def button_sender(ctx, advname, page, msgid = 0):
         label1 = options[0].split("@")[0]
         label2 = options[1].split("@")[0]
         if msgid==0:
-            msg = await ctx.send(embed=discord.Embed(title=advname,
-                                     description=content), components=[[
+            msg = await ctx.send(embed, components=[[
                             Button(label=label1,
                                     custom_id="option_1",
                                     style=ButtonStyle.grey),
@@ -37,8 +37,7 @@ async def button_sender(ctx, advname, page, msgid = 0):
                                     custom_id="option_2",
                                     style=ButtonStyle.grey)]])
         else:
-            await ctx.edit(embed=discord.Embed(title=advname,
-                                     description=content), components=[[
+            await ctx.edit(embed, components=[[
                             Button(label=label1,
                                     custom_id="option_1",
                                     style=ButtonStyle.grey),
@@ -50,8 +49,7 @@ async def button_sender(ctx, advname, page, msgid = 0):
         label2 = options[1].split("@")[0]
         label3 = options[2].split("@")[0]
         if msgid==0:
-            msg = await ctx.send(embed=discord.Embed(title=advname,
-                                     description=content), components=[[
+            msg = await ctx.send(embed, components=[[
                             Button(label=label1,
                                     custom_id="option_1",
                                     style=ButtonStyle.grey),
@@ -62,8 +60,7 @@ async def button_sender(ctx, advname, page, msgid = 0):
                                     custom_id="option_3",
                                     style=ButtonStyle.grey)]])
         else:
-            await ctx.edit(embed=discord.Embed(title=advname,
-                                     description=content), components=[[
+            await ctx.edit(embed, components=[[
                             Button(label=label1,
                                     custom_id="option_1",
                                     style=ButtonStyle.grey),
@@ -79,8 +76,7 @@ async def button_sender(ctx, advname, page, msgid = 0):
         label3 = options[2].split("@")[0]
         label4 = options[3].split("@")[0]
         if msgid==0:
-            msg = await ctx.send(embed=discord.Embed(title=advname,
-                                     description=content), components=[[
+            msg = await ctx.send(embed, components=[[
                             Button(label=label1,
                                     custom_id="option_1",
                                     style=ButtonStyle.grey),
@@ -94,8 +90,7 @@ async def button_sender(ctx, advname, page, msgid = 0):
                                     custom_id="option_4",
                                     style=ButtonStyle.grey)]])
         else:
-            msg = await ctx.edit(embed=discord.Embed(title=advname,
-                                     description=content), components=[[
+            msg = await ctx.edit(embed, components=[[
                             Button(label=label1,
                                     custom_id="option_1",
                                     style=ButtonStyle.grey),
